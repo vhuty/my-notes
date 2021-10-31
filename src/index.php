1bbb579dd36bfe5ff91e82acc6e28f3380e3ea7b
Vladislav Hutych <mandarineye@gmail.com>
2021-10-31 15:05:11 +0200

    Initial commit

---
+++ src/index.php
@@ -0,0 +1,9 @@
+<?
+session_start();
+
+if (!isset($_SESSION['id'])) {
+  header("Location: login.php");
+} else {
+  header("Location: test.php");
+}
+?>
---
+++ src/login.php
@@ -0,0 +1,33 @@
+<?
+session_start();
+
+$users = array(
+  sha1('adminimda@gmail.com') => '70033451de355e8bbbd76d80fdb25fd9b89724fc' //pumpumpumpkin
+);
+
+if (isset($_POST['submit'])) {
+  $raw_email = $_POST['email'];
+  $raw_password = $_POST['password'];
+
+  $email_hash = sha1($raw_email);
+  $password_hash = sha1($raw_password);
+
+  if (isset($users[$email_hash])) {
+    $password = $users[$email_hash];
+
+    if ($password === $password_hash) {
+      $_SESSION['id'] = uniqid();
+
+      header("Location: index.php");
+
+      exit();
+    }
+  }
+
+  echo('<p>User with such email and password not found</p>');
+}
+
+echo("<script>console.log(". json_encode($_POST) .");</script>");
+
+include('login.html');
+?>  
